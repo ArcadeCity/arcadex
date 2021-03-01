695a921996942638f0bbd43c0c5eaa21cc81643c
Christopher David <chris@arcade.city>
2021-02-28 23:51:19 -0600

    Add Arcade UI, create new homepage (#6)
    * ignore npmrc
    
    * add AC ui
    
    * Add homepage and route
    
    * Clean up layout
    
    * Pull in font
    
    * Fix walletconnect styling
    
    * Add new homepage mock
    
    * page title

--- src/components/BasicLayout.js
+++ src/components/BasicLayout.js
@@ -1,8 +1,6 @@
 import { Layout } from 'antd';
 import React from 'react';
-import TopBar from './TopBar';
-import { CustomFooter as Footer } from './Footer';
-const { Header, Content } = Layout;
+const { Content } = Layout;
 
 export default function BasicLayout({ children }) {
   return (
@@ -10,11 +8,7 @@ export default function BasicLayout({ children }) {
       <Layout
         style={{ display: 'flex', minHeight: '100vh', flexDirection: 'column' }}
       >
-        <Header style={{ padding: 0, minHeight: 64, height: 'unset' }}>
-          <TopBar />
-        </Header>
         <Content style={{ flex: 1 }}>{children}</Content>
-        <Footer />
       </Layout>
     </React.Fragment>
   );
