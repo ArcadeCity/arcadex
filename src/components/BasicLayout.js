f20bc5ed8471fb9208ded5223fd757563a963842
Christopher David <chris@arcade.city>
2021-03-01 03:11:48 -0600

    Show navbar on deep pages, not home (#9)

--- src/components/BasicLayout.js
+++ src/components/BasicLayout.js
@@ -1,13 +1,22 @@
 import { Layout } from 'antd';
 import React from 'react';
-const { Content } = Layout;
+import { useLocation } from 'react-router-dom';
+import TopBar from './TopBar';
+const { Header, Content } = Layout;
 
 export default function BasicLayout({ children }) {
+  const location = useLocation();
   return (
     <React.Fragment>
       <Layout
         style={{ display: 'flex', minHeight: '100vh', flexDirection: 'column' }}
       >
+        {location.pathname !== '/' && (
+          <Header style={{ padding: 0, minHeight: 64, height: 'unset' }}>
+            <TopBar />
+          </Header>
+        )}
+
         <Content style={{ flex: 1 }}>{children}</Content>
       </Layout>
     </React.Fragment>
