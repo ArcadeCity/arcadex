31005f4b2da8a8ef936380985329d717c29cf1ba
Christopher David <chris@arcade.city>
2021-02-28 23:12:15 -0600

    Add homepage and route

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ArcadeUI } from '@arcadecity/ui';
+
+const Homepage = () => {
+  return (
+    <ArcadeUI>
+      <p>homepage</p>
+    </ArcadeUI>
+  );
+};
+
+export default Homepage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
-import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import TradePage from './pages/TradePage';
 import OpenOrdersPage from './pages/OpenOrdersPage';
+import Homepage from './pages/Homepage';
 import React from 'react';
 import BalancesPage from './pages/BalancesPage';
 import ConvertPage from './pages/ConvertPage';
@@ -9,7 +10,6 @@ import ListNewMarketPage from './pages/ListNewMarketPage';
 import NewPoolPage from './pages/pools/NewPoolPage';
 import PoolPage from './pages/pools/PoolPage';
 import PoolListPage from './pages/pools/PoolListPage';
-import { getTradePageUrl } from './utils/markets';
 import MinterPage from './pages/MinterPage';
 
 export function Routes() {
@@ -18,9 +18,7 @@ export function Routes() {
       <HashRouter basename={'/'}>
         <BasicLayout>
           <Switch>
-            <Route exact path="/">
-              <Redirect to={getTradePageUrl()} />
-            </Route>
+            <Route exact path="/" component={Homepage} />
             <Route exact path="/market/:marketAddress">
               <TradePage />
             </Route>
